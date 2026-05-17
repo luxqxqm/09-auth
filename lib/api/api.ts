6cf7de7839948187d9aeec6036741a33df81b054
Den Yaseniuk <denasenuk@gmail.com>
2026-05-17 22:58:33 +0200

    add func getMe

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const proxyServerApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_NOTEHUB_TOKEN + "/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true, 
 });
